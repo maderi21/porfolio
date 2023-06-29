e23c92d0cb8cc19beee4cee2a49e6aed67a45297
xkostolanskym <kostolansky.marek@gmail.com>
2023-06-29 21:05:21 +0200

    custom scss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Navbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./custom.scss";
+
 function App() {
   return (
     <div>
-      <p>Hello</p>
+      <Navbar />
     </div>
   );
 }
