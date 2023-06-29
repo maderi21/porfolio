f8cfa86a5a515ad2315232d573c50ea3fe06be0a
xkostolanskym <kostolansky.marek@gmail.com>
2023-06-29 22:42:22 +0200

    Header done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Navbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./custom.scss";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   );
 }
