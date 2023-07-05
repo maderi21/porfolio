d0ad5b0936d01cc1f37ced0f626cc9b0ee0995d8
xkostolanskym <kostolansky.marek@gmail.com>
2023-07-06 00:06:49 +0200

    footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,18 +2,15 @@ import { Col } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <section className="bg-white">
-      <div className="d-flex justify-content-between container">
-        <div className=" col-md">
-          <h5 className="mb-0 pt-5">
-            © DESIGNED BY _MK | MAREK KOSTOLANSKY 2023
-          </h5>
+    <>
+      <hr />
+      <footer className="bg-primary py-3 mt-3">
+        <div className="container text-center text-dark">
+          <p>kostolansky.marek@gmail.com</p>
+          <span>© Designed by _MK | Marek Kostolansky 2023</span>
         </div>
-        <div className="containercol-md">
-          <h5 className="mb-0 p-5">kostolansky.marek@gmail.com</h5>
-        </div>
-      </div>
-    </section>
+      </footer>
+    </>
   );
 };
 
